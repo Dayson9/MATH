c06380d69b225c3734c5791f9dda6a14f40c7892
dayson9 <ainsteinwells@gmail.com>
2024-10-01 20:04:45 +0100

    update

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const dataset = [
+  { input: "( 8 / 4 )", output: "2", e: true },
+  { input: "( 2 * 9 )", output: "18", e: true },
+  { input: "( 2 + 9 )", output: "11", e: true },
+  { input: "( 2 - 9 )", output: "-7", e: true },
   { input: "8 / 4", output: "2", e: true },
   { input: "2 * 9", output: "18", e: true },
   { input: "2 + 9", output: "11", e: true },
   { input: "2 - 9", output: "-7", e: true },
   { input: "y + 1 = 0", output: "y = 0 - 1" },
   { input: "2 y = 0", output: "y = 0 / 2" },
-  { input: "2 / y = 0", output: "y = 0 * 2" },
+  { input: "2 / y = 1", output: "1 * y = 2" },
   { input: "2 - y = 0", output: "- y = 0 + 2" },
+  { input: "1 * y = 2", output: "y = 2 / 1" },
+  { input: "1 + y = 2", output: "y = 2 - 1" },
   { input: "x - 1 = 0", output: "x = 0 + 1" },
   { input: "x + 1 = 0", output: "x = 0 - 1" },
   { input: "x * 1 = 0", output: "x = 0 / 1" },
   { input: "x / 1 = 0", output: "x = 0 * 1" },
   { input: "1 * x = - 0.5", output: "x = - 0.5 / 1" },
-  { input: "- a =", output: "a = -" }
+  { input: "- a =", output: "a = -" },
+  { input: "+ -", output: "-" },
+  { input: "- +", output: "-" }
 ];
 
 // Function to convert a string into a regular expression that matches the input string.
@@ -27,7 +35,7 @@ const toRegex = (inputString) => {
   const regexTokens = tokens.map((token, i) => {
     // If the token is a number or "0", create a regular expression that matches a number.
     if (!(operators.includes(token)) && parseInt(token) || token === "0") {
-      return "\\s+([-][ ]?)?\\d+(\\.\\d+)?";
+      return "\\s*([\-][ ]*)?\\d+(\\.\\d+)?";
     }
     // If the token is an operator including "=", create a regular expression that matches the operator.
     else if (([...operators, "="].includes(token))) {
@@ -54,20 +62,29 @@ const format = (inputString) => {
   const variableRegex = /[0-9]+[a-zA-Z]+/g;
 
   // Define a regular expression for operators.
-  const operatorRegex = /\+|-|\/|\*|=/g;
+  const operatorRegex = /\+|\/|\*|\=/g;
+  // Define regular expressions for brackets
+  const bracketRegex = /\[|\]|\(|\)/;
 
   // Remove all spaces from the input string.
   let formattedString = inputString.replace(/\s/g, "");
 
   // Replace operators with spaces around them.
   formattedString = formattedString.replace(operatorRegex, (operator) => " " + operator + " ");
-
+  
+  
   // Replace variables with spaces between the number and the letter.
   formattedString = formattedString.replace(variableRegex, (extract) => {
     const number = numberRegex.exec(extract)[0];
     const letter = letterRegex.exec(extract)[0];
     return number + " " + letter;
   });
+
+  // Replace brackets with spaces around them.
+  formattedString = formattedString.replace(bracketRegex, (bracket) => " " + bracket + " ");
+  
+  // Remove all double or more spaces
+ formattedString = formattedString.replace(/\s{2,}/g, "");
 
   // Return the formatted string.
   return formattedString;
@@ -119,6 +136,8 @@ const warn = (message) => console.warn(message);
 // Execute the toExecutable function and store the result in the executables variable.
 const executables = toExecutable();
 
+
+
 const evaluateExpr = (str) => {
   const reg = /-?\d+(\.\d+)?[ ](\+|\*|\/|\-){1}[ ]-?\d+(\.\d+)?/g;
 
@@ -152,7 +171,6 @@ const toOutput = (extractedText, data) => {
 
 const stillExecutable = (expression) => {
   let out = false;
-
   for ({ regex } of executables) {
     let reg = new RegExp(regex);
     if (reg.test(expression)) {
@@ -178,9 +196,8 @@ const predict = (input) => {
     for ({ data, regex, e } of executables) {
       const reg = new RegExp(regex),
         test = reg.test(currentInput),
-      matched = reg.exec(currentInput);
+        matched = reg.exec(currentInput);
 
-      match = stillExecutable(currentInput);
       bfr = format(currentInput);
 
       if (test) {
@@ -192,10 +209,11 @@ const predict = (input) => {
       currentInput = evaluateExpr(currentInput);
 
       output.push(currentInput === bfr ? "" : currentInput);
+      match = stillExecutable(currentInput);
     }
   }
 
   return [formattedInput, ...output.filter(Boolean)];
 };
 
-warn(predict("x + 8 = -2").join("\n"));
+warn(predict("4 * 8 + y = 64 / 2 * -8").join("\n\n") + "//");
